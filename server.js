fd97323137fe733f0bc6615ed1a4e2925b023959
MacBook <macbook@MacBooks-MacBook-Pro.local>
2026-03-30 18:29:20 +0530

    allow cords for all

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: '*',
   credentials: true
 }));
 app.use(express.json());
